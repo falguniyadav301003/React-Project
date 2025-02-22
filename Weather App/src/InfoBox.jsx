e36d50cd5a56c669c1fd41d8bf3dcc86b8eebdd2
Falguni Yadav ( Kumkum ) <falguniyadav1430@gmail.com>
2025-02-22 22:37:19 +0530

    Create InfoBox.jsx

---
+++ Weather App/src/InfoBox.jsx
@@ -0,0 +1,40 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+
+export default function InfoBox({ info }) {
+    const INIT_URL = "https://images.unsplash.com/photo-1739467444239-840b9b3c2480?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D";
+    const HOT_URL = "https://images.unsplash.com/photo-1524594081293-190a2fe0baae?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D"; 
+    const COLD_URL = "https://images.unsplash.com/photo-1615226120487-90ec48f5d316?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvbGQlMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+    const RAIN_URL = "https://media.istockphoto.com/id/1257951336/photo/transparent-umbrella-under-rain-against-water-drops-splash-background-rainy-weather-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=sw_CRZcGopaGHDWqtT1M8y64k5uCcq-nro55Bw3YzyQ=";
+
+    return (
+        <div className="InfoBox">
+            <div className="cardContainer">
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={info.humidity > 80 ? RAIN_URL : info.temp_c > 15 ? HOT_URL : COLD_URL }
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {info.city}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+                            <p> Date & Time: {info.time}</p>
+                            <p> Temperature: {info.temp_c}&deg;C </p>
+                            <p> Condition: {info.condition.text} </p>  
+                            <img src={info.condition.icon} alt="Weather icon" />  
+                            <p> Cloud: {info.cloud} </p>
+                            <p> Humidity: {info.humidity}</p>
+                            <p> Country: {info.country}</p>
+                            <p> The weather can be described as <i>{info.condition.text}</i> and feels like {info.feelslike}&deg;C </p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
+}
